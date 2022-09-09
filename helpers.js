c1f0c0bced7d7f6dc86a273be073cafccca7213e
John Page <john.page@10gen.com>
2022-09-09 10:13:45 +0100

    Create helpers.js
    Version 1

---
+++ helpers.js
@@ -0,0 +1,14 @@
+const singleargops = ['eq','gt','gte','lt','lte','ne',
+'not','nor','or','exists','type','expr','jsonSchema',
+'mod','text','elemMatch','size','comment','rand','regex']
+
+// Convert op(arg) to { $op : arg }
+for(const op of singleargops) {
+   global[op] = (value)=>({[`\$${op}`]:value})
+}
+
+// Convert op(arg21,arg2,arg3) to { $op : [ arg1,arg2,arg3]}
+const multiargops = ['and','or','in','nin','all']
+for(const op of multiargops) {
+  global[op] = (...values)=>({[`\$${op}`]:values})
+}
